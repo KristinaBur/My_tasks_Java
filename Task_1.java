d0bd2edbb6fe5a259b97541cad6ee82bdf85a419
«Kristina» <kristinka007_92@mail.ru>
2023-04-14 10:39:35 +0300

    Задача 1

---
+++ Task_1.java
@@ -0,0 +1,24 @@
+// Task_1
+// Улитка ползёт по вертикальному шесту высотой h метров, поднимаясь за день на a метров, а за ночь спускаясь на b метров. На какой день улитка доползёт до вершины шеста?
+// Программа получает на вход натуральные числа h, a, b. Гарантируется, что a>b.
+// Программа должна вывести одно натуральное число.
+// Sample Input 1:
+// 10
+// 3
+// 2
+// Sample Output 1:
+// 8
+// class MyProgram{
+// public static void main(String[] args){
+// }
+// }
+
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int h = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int res = h/(a-b);
+        System.out.println(res);
+    }
